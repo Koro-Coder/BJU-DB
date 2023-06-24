fac4cb3eccc92c18a1c529c910b78a5327595a8a
Ashu <abhishekaggarwal2801@gmail.com>
2023-06-25 03:31:40 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SignInPage from "./SignInPage";
+import "./App.css";
+import HomePage from "./HomePage";
+import SelectedCompanyPage from "./SelectedCompanyPage";
+import DataEntryPage from "./DataEntryPage";
+import GenerateReport from "./GenerateReport";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SignInPage />
+      <HomePage />
+      <SelectedCompanyPage />
+      <DataEntryPage />
+      <GenerateReport />
+    </>
   );
 }
 
